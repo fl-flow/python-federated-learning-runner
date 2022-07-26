eecf5b884028d577a18a64d66c98264e5f634635
shaojiajun314 <63770117@qq.com>
2022-07-26 19:00:23 +0800

    task process 2

--- fl_component/processing/processing.py
+++ fl_component/processing/processing.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from pickle import dumps
 from base64 import b64encode
 from collections.abc import Iterable
@@ -24,6 +25,7 @@ class Process():
         self.stream = stream
         if stream:
             assert isinstance(data, Iterable), 'data required iterable on mode stream'
+        self.split = f'-------------------{uuid4().hex}'
 
     @cached_property
     def w(self):
@@ -43,18 +45,33 @@ class Process():
         )
         self.w.flush()
         d = self.r.readline()
+        print(d)
         self.__write_data()
 
     def __write_data_obj(self):
+        split_ = self.split + '\n'
+        self.w.write(
+            f'{len(split_)}\n'
+        )
+        self.w.write(split_)
         d = self.encode(dumps(self.data))
         self.w.write(
             f'{len(d)}\n'
         )
         self.w.write(d + '\n')
         self.w.flush()
+        self.__read_data()
 
     def __write_data(self):
         if self.stream:
             # TODO:
             return
         self.__write_data_obj()
+
+    def __read_data(self):
+        while 1:
+            count = self.r.readline().strip()
+            if not count:
+                break
+            d = self.r.read(int(count))
+            print(d)
---
+++ task_processing.py
@@ -0,0 +1,8 @@
+from fl_component.processing.processing import Process
+
+
+if __name__ == '__main__':
+    f = open('ddd', 'w')
+    while 1:
+        f.write(input() + "\n")
+        f.flush()
